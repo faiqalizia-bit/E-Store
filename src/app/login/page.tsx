5fd6fb95525690928557327e1f5cc0154c605fdf
faiqalizia-bit <faiqalizi@gmail.com>
2026-04-29 01:04:05 +0500

    creat auth pages with local storage

---
+++ src/app/login/page.tsx
@@ -0,0 +1,123 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { User } from "../register/page";
+function Login() {
+   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [message, setMessage] = useState("");
+const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleLogin = () => {
+  setError("");
+  setMessage("");
+
+  const storedUser = localStorage.getItem("users");
+  console.log("🚀 ~ handleLogin ~ storedUser:", storedUser)
+
+  if (!storedUser) {
+    setError("No account found. Please register first.");
+    return;
+  }
+
+  const user = JSON.parse(storedUser);
+  console.log("🚀 ~ handleLogin ~ user:", user)
+
+  const foundUser = user.find(
+    (u:User) => u.email === email && u.password === password
+  );
+
+  if (foundUser) {
+    localStorage.setItem("isLoggedIn", "true");
+
+    setMessage("Login successful! Redirecting...");
+
+  
+      router.push("/home");
+  
+
+  } else {
+    setError("Invalid email or password");
+  }
+};
+
+  
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-300 to-blue-600 relative overflow-hidden">
+      {/* Card */}
+      <div className="bg-white/10 rounded-2xl w-[350px] text-white shadow-2xl border-white/20 p-6">
+        {/* <h2 className="text-sm opacity-80">Your logo</h2> */}
+        <h1 className="text-3xl font-bold mb-6">Login</h1>
+
+        {/* Email */}
+        <div className="mb-4">
+         {error && (
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
+            {error}
+          </div>
+        )}
+
+        {message && (
+          <div className="text-center text-gray-500">No messages yet</div>
+        )}
+
+          <label className="text-sm">Email</label>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="username@gmail.com"
+            className="w-full mt-1 p-2 rounded bg-white/80 text-black outline-none"
+          />
+        </div>
+
+        <div className="mb-2">
+          <label className="text-sm">Password</label>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="enter your Password"
+            className="w-full mt-1 p-2 rounded bg-white/80 text-black outline-none"
+          />
+        </div>
+
+        <p className="text-xs mb-4 cursor-pointer hover:underline">
+          Forgot Password?
+        </p>
+
+        <button   onClick={handleLogin} className="w-full bg-blue-900 hover:bg-blue-800 transition p-2 rounded font-semibold">
+          Sign in
+        </button>
+
+        {/* <p className="text-center text-sm my-4 opacity-80">
+          or continue with
+        </p> */}
+
+        {/* Social buttons */}
+        {/* <div className="flex justify-between gap-3">
+          <button className="flex-1 bg-white text-black p-2 rounded">
+            G
+          </button>
+          <button className="flex-1 bg-white text-black p-2 rounded">
+            GH
+          </button>
+          <button className="flex-1 bg-white text-black p-2 rounded">
+            f
+          </button>
+        </div> */}
+
+        <p className="text-center text-sm mt-4">
+          Don’t have an account?{" "}
+          <Link href="/register" className="underline cursor-pointer">
+            Register for free
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,193 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import React from "react";
+export interface User {
+    name:string,
+    email:string,
+    password:string
+}
+function Register() {
+    const router = useRouter();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleRegister = () => {
+    setError("");
+    setSuccess("");
+
+  
+    if (!name || !email || !password || !confirmPassword) {
+      setError("All fields are required");
+      return;
+    }
+
+    if (password.length < 6) {
+      setError("Password must be at least 6 characters");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+const existingUsers = localStorage.getItem("users");
+
+  const users = existingUsers ? JSON.parse(existingUsers) : [];
+
+ 
+  const userExists = users.find((u:User ) => u.email === email);
+
+  if (userExists) {
+    setError("User already exists with this email");
+    return;
+  }
+
+  
+  const newUser = {
+    name,
+    email,
+    password,
+  };
+
+  
+  users.push(newUser);
+
+
+  localStorage.setItem("users", JSON.stringify(users));
+
+  setSuccess("Registration successful! Redirecting...");
+
+  setTimeout(() => {
+    router.push("/home");
+  }, 1500);
+};
+//     const existingUser = localStorage.getItem("user");
+
+//     if (existingUser) {
+//       const user = JSON.parse(existingUser);
+
+//       if (user.email === email) {
+//         setError("User already exists with this email");
+//         return;
+//       }
+//     }
+
+   
+//     const newUser = {
+//       name,
+//       email,
+//       password,
+//     };
+
+//     localStorage.setItem("user", JSON.stringify(newUser));
+
+//     setSuccess("Registration successful! Redirecting...");
+
+//     setTimeout(() => {
+//       router.push("/home");
+//     }, 1500);
+//   };
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-300 to-blue-600 relative overflow-hidden">
+      {/* 
+      {/* Card */}
+      <div className="backdrop-blur-lg bg-white/10 border border-white/20 rounded-2xl p-8 w-[380px] text-white shadow-2xl">
+       
+       
+        <h1 className="text-3xl font-bold mb-6">Register</h1>
+
+        {/* Name */}
+        <div className="mb-4">
+                     {error && (
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
+            {error}
+          </div>
+        )}
+
+        {success && (
+          <div className="text-center text-gray-500">No messages yet</div>
+        )}
+          <label className="text-sm">Full Name</label>
+          <input
+            type="text"
+            placeholder="Enter Full Name"
+             onChange={(e) => setName(e.target.value)}
+            className="w-full mt-1 p-2 rounded bg-white/80 text-black outline-none"
+          />
+        </div>
+
+        {/* Email */}
+        <div className="mb-4">
+          <label className="text-sm">Email</label>
+          <input
+            type="email"
+             onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter Your E-mail"
+            className="w-full mt-1 p-2 rounded bg-white/80 text-black outline-none"
+          />
+        </div>
+
+        {/* Password */}
+        <div className="mb-4">
+          <label className="text-sm">Password</label>
+          <input
+            type="password"
+             onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="w-full mt-1 p-2 rounded bg-white/80 text-black outline-none"
+          />
+        </div>
+
+        {/* Confirm Password */}
+        <div className="mb-4">
+          <label className="text-sm">Confirm Password</label>
+          <input
+            type="password"
+             onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Confirm Password"
+            className="w-full mt-1 p-2 rounded bg-white/80 text-black outline-none"
+          />
+        </div>
+
+        {/* Button */}
+        <button  onClick={handleRegister} className="w-full bg-blue-900 hover:bg-blue-800 transition p-2 rounded font-semibold">
+          Sign up
+        </button>
+
+        {/* <p className="text-center text-sm my-4 opacity-80">
+          or continue with
+        </p> */}
+
+        {/* Social buttons */}
+        {/* <div className="flex justify-between gap-3">
+          <button className="flex-1 bg-white text-black p-2 rounded">
+            G
+          </button>
+          <button className="flex-1 bg-white text-black p-2 rounded">
+            GH
+          </button>
+          <button className="flex-1 bg-white text-black p-2 rounded">
+            f
+          </button>
+        </div> */}
+
+        <p className="text-center text-sm mt-4">
+          Already have an account?{" "}
+          <Link href="/" className="underline cursor-pointer">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
